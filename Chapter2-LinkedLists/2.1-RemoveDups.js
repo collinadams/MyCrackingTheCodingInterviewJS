34b7a53c39adfa28ad3b92fbc939198051fc36b0
Collin Adams <collinadams04@gmail.com>
2016-06-15 08:46:19 -0700

    Remove duplicates from Singly Linked List

--- Chapter2-LinkedLists/2.1-RemoveDups.js
+++ Chapter2-LinkedLists/2.1-RemoveDups.js
@@ -4,9 +4,21 @@ Write code to remove duplicates from an unsorted linked list.
 
 function removeDuplicates(firstElement) { //assumes input is the head of the LinkedList (and that it is a singly linked list)
 	//first element is not a duplicate
+  var values = [firstElement.value];
+  var current = firstElement.next;
+  var previous = firstElement;
 
+  while(current){
+    if(values.indexOf(current.value) >= 0){
+      previous.next = current.next;
+    }else{
+      values.push(current.value);
+      previous = current;
+    }
+    current = current.next;
+  }
 
-
+  return firstElement;
 }
 
 /*
